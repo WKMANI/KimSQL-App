906eb269018179e204beeda312d13916d06af351
wkmani <wilfredmburu08@gmail.com>
2018-11-29 18:55:13 +0300

    Creating a fragment to connect to an activity

---
+++ app/src/main/java/cool/modcom/com/kimsql_app/HomeFragment.java
@@ -0,0 +1,25 @@
+package cool.modcom.com.kimsql_app;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class HomeFragment extends Fragment {
+    //Fragments can be attached in an activity
+    //to create a Fragment onCreateView is used as the class entry point
+    //activities use onCreate itself as class entry point
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        //connect this fragment to its layout
+
+        return;
+
+    }
+}
